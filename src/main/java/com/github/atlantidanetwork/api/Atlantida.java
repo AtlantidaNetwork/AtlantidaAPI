74dbad2533b2c9778026993489d2bed0f88563a0
AtlantidaNetwork <atlantidanetwork@outlook.com>
2019-12-24 01:19:15 -0300

    AtlantidaAPI
    Fix Main null.

--- src/main/java/com/github/atlantidanetwork/api/Atlantida.java
+++ src/main/java/com/github/atlantidanetwork/api/Atlantida.java
@@ -1,4 +1,20 @@
 package com.github.atlantidanetwork.api;
 
-public class Atlantida {
+import com.github.atlantidanetwork.api.lib.plugin.AtlantidaPlugin;
+
+public class Atlantida extends AtlantidaPlugin {
+
+    public Atlantida() {
+        super("api");
+    }
+
+    @Override
+    public void onEnable() {
+        msg("§6[AtlantidaAPI] §3Api habilitada.");
+    }
+
+    @Override
+    public void onDisable() {
+        msg("§6[AtlantidaAPI] §3Api desabilitada.");
+    }
 }
